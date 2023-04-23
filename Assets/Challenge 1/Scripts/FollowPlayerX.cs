ad911aa20cbab9d15f0d68aafcf04c9df5378e61
Kordeliia <lalvmun@hotmail.com>
2023-04-23 04:28:50 +0200

    Finish airplane

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset = new Vector3 (0, 5, -5);
+    public Vector3 offset = new Vector3 (0, 5, -6);
 
     // Start is called before the first frame update
     void Start()
--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
+    public static PlayerControllerX sharedInstance;
     public float speed;
     public float rotationSpeed;
+    public float horizontalInput;
     public float verticalInput;
 
+    private void Awake()
+    {
+        sharedInstance = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -17,13 +23,17 @@ public class PlayerControllerX : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        // get the user's horizontal input
+        horizontalInput = Input.GetAxis("Horizontal");
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
 
-        // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
-
+        transform.Translate(speed * Time.deltaTime * Vector3.forward);
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime * verticalInput);
+
+        //tilt the plane up/down based on up/down arrow Keys
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime * horizontalInput);
+
     }
 }
